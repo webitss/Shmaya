10f4e67ce063060385fab637333c6d539a83675c
batshevavwebit <bat-sheva.v@webit-sys.com>
2017-11-29 08:46:16 +0200

    שינויי עיצוב

--- shmaaya/src/app/container.component.ts
+++ shmaaya/src/app/container.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'container',
   template: `
-<<<<<<< HEAD
+
+  
+  <div class="header-main">
   <div class="container">
+  <div class="logo-img"><img src="../assets/pictures/logo.png"/></div>
   <ul class="nav nav-pills">
       <li routerLink="/order"  routerLinkActive="active">הזמנות</li>
       <li routerLink="/alfon"  routerLinkActive="active">אלפון</li>
@@ -12,21 +15,14 @@ import { Component } from '@angular/core';
       <li routerLink="/properties"  routerLinkActive="active">הגדרות</li>
       <li routerLink="/reports"  routerLinkActive="active">דוחות</li>
   </ul>
-=======
-  <div dir="rtl">
-  <nav>
-      <a routerLink="/order"  routerLinkActive="active" >הזמנות</a> |
-      <a routerLink="/alfon"  routerLinkActive="active">אלפון</a> |
-      <a routerLink="/sendmessages"  routerLinkActive="active">שליחת הודעות</a> |
-      <a routerLink="/properties"  routerLinkActive="active">הגדרות</a> |
-      <a routerLink="/reports"  routerLinkActive="active">דוחות</a> |
-  
-  </nav>
->>>>>>> 9eac479607d8c8a7fb7cc80e276e70e9a48175fe
+  </div>
+  </div>
+ 
     <div >
        <router-outlet></router-outlet> 
     </div>
-</div>
+
+
     
   `,
   styles: []
