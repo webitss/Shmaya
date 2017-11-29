6268e93e230b3ad6fa165e9adddca69ec296ef0f
batshevavwebit <bat-sheva.v@webit-sys.com>
2017-11-29 09:01:46 +0200

    שינויי

--- shmaaya/src/app/Reports/reports.components.ts
+++ shmaaya/src/app/Reports/reports.components.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'reports',
   template: `
-  <div dir="rtl">
+  <div class="container">
   <a routerLink="serviceproviders_rep"  routerLinkActive="active">דוח נותני שירות</a>|
   <a routerLink="reciveservice_rep"  routerLinkActive="active">דוח מקבלי שירות</a>|
   <a routerLink="products_rep"  routerLinkActive="active">דוח מוצרים</a>|
