fe871eee658e2d1997618a052ff3477d6a606693
shulamitawebit <34027837+shulamitawebit@users.noreply.github.com>
2017-12-05 09:43:45 +0200

    הוספות

---
+++ Client/Scripts/Controllers/Settings/EligibilityTableCtrl.js
@@ -0,0 +1,16 @@
+﻿"use strict"
+companionApp.controller('EligibilityTableCtrl', ['$scope', '$rootScope', 'connect', '$timeout', '$filter', 'alerts', 'createDialog', '$uibModal', 'codeTablesId',
+	function ($scope, $rootScope, connect, $timeout, $filter, alerts, createDialog, $uibModal, codeTablesId) {
+		$scope.gridIdentity = 'ordersList';
+		$scope.columns = [
+			{
+				fieldName: 'iId',
+				title: 'עריכה',
+				weight: 0.9,
+				filter: false,
+				sort: false
+			},
+			{ title: 'סוג זכאות', fieldName: 'dtTimeBegin' },
+			{ title: 'מספר שעות זכאות', fieldName: 'nvAvailability' }
+		];
+	}]);
--- Client/Scripts/Controllers/Settings/SettingTabsCtrl.js
+++ Client/Scripts/Controllers/Settings/SettingTabsCtrl.js
@@ -2,7 +2,7 @@
 companionApp.controller('SettingTabsCtrl', ['$scope', '$rootScope', 'connect', '$location', '$filter', '$timeout', 'codeTablesName', 'codeTablesId', function ($scope, $rootScope, connect, $location, $filter, $timeout, codeTablesName, codeTablesId) {
     $scope.settingsActiveTab = 0;
     $scope.tabs = [
-       { title: 'טבלת זכאות', content: 'Partials/Pages/Settings/Settings.html', disabled: $rootScope.user.iUserType != codeTablesId.permissionType.systemAdministrator},
+		{ title: 'טבלת זכאות', content: 'Partials/Pages/Settings/EligibilityTable.html', disabled: $rootScope.user.iUserType != codeTablesId.permissionType.systemAdministrator},
         { title: 'סל תקשורת', content: 'Partials/Pages/Settings/MoreSettings.html', disabled: $rootScope.user.iUserType != codeTablesId.permissionType.systemAdministrator },
 		{ title: 'תשלומים', content: 'Partials/Pages/Settings/Permissions.html', disabled: $rootScope.user.iUserType != codeTablesId.permissionType.systemAdministrator },
 		{ title: 'מוצרים', content: 'Partials/Pages/Settings/Permissions.html', disabled: $rootScope.user.iUserType != codeTablesId.permissionType.systemAdministrator },
