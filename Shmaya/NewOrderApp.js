00e2cd20c9a2a28f690ce01146d49f2429b4446d
sarakwebit <saram@webit-sys.com>
2018-01-17 13:08:17 +0200

    הדף הבודד

---
+++ Shmaya/NewOrderApp.js
@@ -0,0 +1,17 @@
+﻿"use strict"
+var a = 2;
+var orderApp = angular.module('orderApp', ['ngRoute', 'fundoo.services']);
+orderApp.config(['$routeProvider', '$compileProvider', function ($routeProvider) {
+    $routeProvider
+		.when('/', {
+		    templateUrl: 'shmaya.html' + appVersionParameter,
+		    controller: 'NewOrderCtrl'
+		})
+        .when('/shmaya', {
+            templateUrl: 'shmaya.html' + appVersionParameter,
+            controller: 'NewOrderCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
--- Shmaya/NewOrderCtrl.js
+++ Shmaya/NewOrderCtrl.js
@@ -1,9 +1,36 @@
 ﻿"use strict"
-companionApp.controller('NewOrderCtrl', ['$scope', '$rootScope', '$timeout', 'connect', '$filter', '$location', 'alerts',
+orderApp.controller('NewOrderCtrl', ['$scope', '$rootScope', '$timeout', 'connect', '$filter', '$location', 'alerts',
     function ($scope, $rootScope, $timeout, connect, $filter, $location, alerts) {
+        alert();
+
+        $scope.successSend = false;
+
+        $scope.maxDate = new Date();
+
+        $scope.showWeeks = false;
+
+        $scope.popup = {
+            opened: false
+        };
+
+        $scope.open = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.popup.opened = true;
+        };
+
+        $scope.disabled = function (date, mode) {
+            return (mode === 'day' && (date.getDay()));
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
 
         $scope.getData = function () {
-            connect.post(true, 'GetUserCodeTables', { iUserId: $rootScope.user.iUserId }, function (result) {
+            OrderConnect.post(true, 'GetUserCodeTables', { iUserId: $rootScope.user.iUserId }, function (result) {
                 $scope.codeTables = result;
                 $scope.translateTypeList = $filter('filter')(result, { Key: 'translateType' }, true)[0].Value;
                 $scope.orderTypeList = $filter('filter')(result, { Key: 'orderType' }, true)[0].Value;
@@ -14,11 +41,11 @@ companionApp.controller('NewOrderCtrl', ['$scope', '$rootScope', '$timeout', 'co
         };
 
         $scope.getTranslators = function (iTypeTranslation) {
-            connect.post(true, 'GetUsers', { iUserType: 3, iStatusId: 0, iTypeTranslation: $scope.iTypeTranslation }, function (result) {
+            OrderConnect.post(true, 'GetUsers', { iUserType: 3, iStatusId: 0, iTypeTranslation: $scope.iTypeTranslation }, function (result) {
                 if (result && result.length > 0)
                     $scope.ABCBookProviders = result;
                 else
-                    alerts.alert('ארעה שגיאה בלתי צפויה');
+                    OrderAlerts.alert('ארעה שגיאה בלתי צפויה');
             });
         };
 
