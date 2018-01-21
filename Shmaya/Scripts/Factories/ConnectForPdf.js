e481b0584c90c05fbf1279d51cece43e8cd88d34
sarakwebit <saram@webit-sys.com>
2018-01-21 13:05:27 +0200

    PDF

--- Shmaya/Scripts/Factories/ConnectForPdf.js
+++ Shmaya/Scripts/Factories/ConnectForPdf.js
@@ -1,15 +1,60 @@
 "use strict";
-appFWPdf.factory("ConnectForPdf", ['$http', function ($http) {
-    var urlBase = 'http://localhost:14733/Service1.svc/';
+appMessagePdf.factory("ConnectForPdf", ['$http', function ($http) {
+    var urlBase = 'http://localhost/ShmayaService/Service.svc/';
     //qa
-  //  var urlBase = 'http://qa.webit-track.com/FWSalryWS/Service1.svc/';
+    //  var urlBase = 'http://qa.webit-track.com/FWSalryWS/Service1.svc/';
+
+    function dateConvectionLoop(data, isPost) {
+        if (!angular.isObject(data)) {
+            var prop = data;
+            if (isPost == false && angular.isString(prop) && prop.indexOf("/Date(") > -1) {
+
+                var date = prop.substring(prop.indexOf("(") + 1, (prop.indexOf("+") != -1 ? prop.indexOf("+") : prop.indexOf(")")));
+                prop = new Date(parseInt(date));
+            }
+            else if (isPost && angular.isDate(prop)) {
+                prop = '/Date(' + Date.UTC(prop.getFullYear(), prop.getMonth(), prop.getDate(), prop.getHours(), prop.getMinutes()) + ')/';
+            }
+            //if (prop == "PT0S")
+            //    prop = null;
+            return prop;
+        }
+        var isArr = angular.isArray(data);
+        if (isArr)
+            var nData = [];
+        else
+            var nData = {};
+        angular.forEach(data, function (prop, key) {
+            if (isPost == false && angular.isString(prop) && prop.indexOf("/Date(") > -1) {
+
+                var date = prop.substring(prop.indexOf("(") + 1, (prop.indexOf("+") != -1 ? prop.indexOf("+") : prop.indexOf(")")));
+                prop = new Date(parseInt(date));
+            }
+            else if (isPost && angular.isDate(prop)) {
+                prop = '/Date(' + Date.UTC(prop.getFullYear(), prop.getMonth(), prop.getDate(), prop.getHours(), prop.getMinutes()) + ')/';
+            }
+            else if (angular.isObject(prop) || angular.isArray(prop))
+                prop = dateConvectionLoop(prop, isPost);
+            //if (prop == "PT0S")
+            //    prop = null;
+            if (isArr)
+                nData.push(prop);
+            else
+                nData[key] = prop;
+        });
+        return nData;
+    }
+
     return {
+
         post: function (actionName, data, SuccessFunc, ErrorFunc) {
+            var data = dateConvectionLoop(data, true);
             $http.post(urlBase + actionName, data)
                         .success(function (result) {
                             SuccessFunc(result);
                         }).error(function (result) {
-                            ErrorFunc(result);
+                            if (ErrorFunc)
+                                ErrorFunc(result);
                         });
         },
         get: function (actionName, SuccessFunc, ErrorFunc) {
@@ -17,7 +62,8 @@ appFWPdf.factory("ConnectForPdf", ['$http', function ($http) {
                 .success(function (result) {
                     SuccessFunc(result);
                 }).error(function (result) {
-                    ErrorFunc(result);
+                    if (ErrorFunc)
+                        ErrorFunc(result);
                 });
         },
     };
